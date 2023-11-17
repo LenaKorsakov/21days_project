9596ebf257e8e127a06a3b7742416b896473b0b6
ElenaKorsakova <lenawwka@gmail.com>
2023-11-17 13:25:39 +0100

    Small fixes

--- src/components/Days/Days.jsx
+++ src/components/Days/Days.jsx
@@ -26,6 +26,7 @@ function Days({
   const firstDayOfHabit = dayjs(new Date(habit.start_day)).startOf('day');
   const today = dayjs();
   const lastDayOfHabit = firstDayOfHabit.add(AMOUNT_OF_DAYS, 'day');
+  console.log(lastDayOfHabit);
 
   const checkIsTodayLastDayOfHabit = () => {
     const isLastDay = dayjs(lastDayOfHabit).isToday();
